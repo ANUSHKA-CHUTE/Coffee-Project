725f410d9c5a59df93896ec01e449f26e148aca7
Vedant-Wedekar <vedantwedekar@gmail.com>
2025-02-26 14:54:54 +0530

    header no - 2 completed

--- Eren/my-app/src/App.jsx
+++ Eren/my-app/src/App.jsx
@@ -1,11 +1,12 @@
 // App.jsx
 import React from 'react'
 import Marq from "./components/Marq"
-
+import Header from "./components/Header"
 const App = () => {
   return (
     <div>
       <Marq/>
+      <Header/>
     </div>
   )
 }
