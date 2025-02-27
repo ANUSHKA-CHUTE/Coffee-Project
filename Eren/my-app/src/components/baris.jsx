b44fd5ba199c8a632af848fb95de240e9f7453ab
Vedant-Wedekar <vedantwedekar@gmail.com>
2025-02-28 05:22:58 +0530

    starts

---
+++ Eren/my-app/src/components/baris.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+
+const baris = () => {
+  return (
+    <div className='h-[500px] w-full flex justify-center items-center'>
+
+       <div className="flex justify-center items-center">
+
+
+
+        <div className="one grid ">
+        <div className="grid">
+            <div className="tit"></div>
+            <div className="det"></div>
+        </div>
+        <div className="grid">
+            <div className="tit"></div>
+            <div className="det"></div>
+        </div>
+            
+        </div>
+
+
+
+
+        <div className="two"></div>
+
+
+
+
+        <div className="three">
+        <div className="grid">
+            <div className="tit"></div>
+            <div className="det"></div>
+        </div> <div className="grid">
+            <div className="tit"></div>
+            <div className="det"></div>
+        </div>
+       
+
+        </div>
+
+
+
+
+
+       </div>
+    </div>
+  )
+}
+
+export default baris
