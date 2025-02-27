37d05cc78ce06bcd16a78c58a87162f2535ddf57
Vedant-Wedekar <vedantwedekar@gmail.com>
2025-02-28 05:22:40 +0530

    little

--- Eren/my-app/src/components/Mtwo.jsx
+++ Eren/my-app/src/components/Mtwo.jsx
@@ -5,6 +5,9 @@ const Mtwo = () => {
     <div className='h-[500px] w-full grid place-items-center '>
       
       <div className="rer">coffee  ?</div>
+      <div className="h-[300px] w-full bg-amber-200 ">
+        BEST COFFEE BEANS
+      </div>
     </div>
   )
 }
