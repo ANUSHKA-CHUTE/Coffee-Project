032464b54f7d501db6b884868cb06b239a98af72
Vedant-Wedekar <vedantwedekar@gmail.com>
2025-02-27 20:27:01 +0530

    new component

---
+++ Eren/my-app/src/components/Mtwo.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Mtwo = () => {
+  return (
+    <div className='h-[500px] w-full grid place-items-center '>
+      
+    </div>
+  )
+}
+
+export default Mtwo
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
