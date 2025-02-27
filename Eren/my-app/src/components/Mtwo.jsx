c2024508ab11be335e6145a0fbaa418225fc9714
Vedant-Wedekar <vedantwedekar@gmail.com>
2025-02-27 21:03:03 +0530

    coffee  ? font

--- Eren/my-app/src/components/Mtwo.jsx
+++ Eren/my-app/src/components/Mtwo.jsx
@@ -4,10 +4,10 @@ const Mtwo = () => {
   return (
     <div className='h-[500px] w-full grid place-items-center '>
       
+      <div className="rer">coffee  ?</div>
     </div>
   )
 }
-
 export default Mtwo
 
 
