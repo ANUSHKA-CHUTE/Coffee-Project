8e7b14b5e37d975b56ce41e5b1d2f6890010c6bb
Vedant-Wedekar <vedantwedekar@gmail.com>
2025-03-06 16:53:55 +0530

    new element

---
+++ Eren/my-app/src/components/Footer.jsx
@@ -0,0 +1,21 @@
+// import React from 'react'
+
+// const Footer = () => {
+//   return (
+//     <div className='grid'>
+//        <div className="flex justify-center items-center">
+//         <div className=""></div>
+//         <div className="">Pvt__.ved</div>
+//         <div className="line"></div>
+//         <div className="">FOLLOW US ON INSTAGRAM    </div>
+//         </div> 
+
+
+//         <div className="flex justify-center items-center">
+//             <div className="h-[350px] w-[350px]"><img src={e}></img></div>
+//         </div>
+//     </div>
+//   )
+// }
+
+// export default Footer
---
+++ Eren/my-app/src/components/Image.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Images = () => {
+  return (
+    <div className='grid h-[600px]'>
+       <div className="flex justify-center items-center">
+        <div className=""></div>
+        <div className="">Pvt__.ved</div>
+        <div className="line">-------------------------------------------------</div>
+        <div className="">FOLLOW US ON INSTAGRAM    </div>
+        </div> 
+
+
+        <div className="flex justify-center items-center">
+            {/* <div className="h-[350px] w-[350px]"><img src={e}></img></div> */}
+        </div>
+    </div>
+  )
+}
+
+export default Images
